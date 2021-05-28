e197e8df470c150606847c1d3be7144f36ee107a
boldenst <oliv045r@edu.easj.dk>
2021-05-28 14:06:21 +0200

    header/aside/fooder/menu
    done

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$("#burger-button").click(function(){
+    $("#burger-menu").toggleClass("burger-menu--show");
+  });
+
+$("#burger-close").click(function(){
+    $("#burger-menu").toggleClass("burger-menu--show");
+  });
