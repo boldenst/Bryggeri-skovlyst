0bd484064fe782ea0a1cac3ff0e3f88083eaa7f7
Hjorthex <74530828+Hjorthex@users.noreply.github.com>
2021-05-28 16:03:25 +0200

    Added buttons
    W. global styling

--- js/main.js
+++ js/main.js
@@ -5,3 +5,13 @@ $("#burger-button").click(function(){
 $("#burger-close").click(function(){
     $("#burger-menu").toggleClass("burger-menu--show");
   });
+
+  $(document).ready(function() {
+    $('.btn-red__global-styling').click(function() {
+      $('.btn-red__global-styling').toggleClass('btn-red__global-styling--gold')
+      $('.unfold__global').toggleClass('unfold__global--show')
+      $('.btn-red__text').toggleClass('btn-red__text--gold')
+      $('.btn-red__arrow-container').toggleClass('btn-red__arrow-container--gold')
+      $('.btn-red__arrow').toggleClass('btn-red__arrow--gold')
+    });
+});
